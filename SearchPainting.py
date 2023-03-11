6dafc6925b8095a0ac3df582894c38b3241c24eb
amne ghazale <amne.sh,94@hotmail.com>
2023-03-11 14:32:27 +0200

    commit and push All

--- SearchPainting.py
+++ SearchPainting.py
@@ -12,42 +12,41 @@ service_obj=Service("../ChromeDriver/chromedriver.exe")
 driver=webdriver.Chrome(service=service_obj, options=chrome_options)
 driver.maximize_window()
 
-def RunSearchTest(search_string):
-    driver.get("http://127.0.0.1:5000/")
-    driver.find_element(By.ID, "searchBar").send_keys(search_string)
-    time.sleep(3)
-
-    driver.find_element(By.ID, "submit").click()
-
+#Search painting By write one word
+driver.get("http://127.0.0.1:5000/")
+driver.find_element(By.ID, "searchBar").send_keys("american")
 time.sleep(3)
-
-# Search painting by writing one word
-
-RunSearchTest("american")
+driver.find_element(By.ID, "submit").click()
 
 time.sleep(3)
 
 # Search painting by writing the first letters
-
-RunSearchTest("am")
-
+driver.get("http://127.0.0.1:5000/")
+driver.find_element(By.ID, "searchBar").send_keys("am")
+time.sleep(3)
+driver.find_element(By.ID, "submit").click()
 time.sleep(3)
 
 # Search painting by writing the full name in small letters
-
-RunSearchTest("american gothic")
-
+driver.get("http://127.0.0.1:5000/")
+driver.find_element(By.ID, "searchBar").send_keys("american gothic")
+time.sleep(3)
+driver.find_element(By.ID, "submit").click()
 time.sleep(3)
 
 # Search painting by writing the full name in capital letters
-
-RunSearchTest("AMERICAN GOTHIC")
-
+driver.get("http://127.0.0.1:5000/")
+driver.find_element(By.ID, "searchBar").send_keys("AMERICAN GOTHIC")
+time.sleep(3)
+driver.find_element(By.ID, "submit").click()
 time.sleep(3)
 
 # Search painting by writing the name of the painting in small and capital letters
+driver.get("http://127.0.0.1:5000/")
+driver.find_element(By.ID, "searchBar").send_keys("aMerIcan GOtHic")
+time.sleep(3)
+driver.find_element(By.ID, "submit").click()
 
-RunSearchTest("aMerIcan GOtHic")
 
 
 
